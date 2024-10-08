98b7aeb1c173b7f3782f236acba3cb7e0bc2b656
ArpanaM <muriki.arpana@gmail.com>
2024-10-08 15:01:58 +0530

    Learning colors

--- 02-print.sh
+++ 02-print.sh
@@ -8,3 +8,8 @@ echo -e "\e[33mHello in Yellow Color\e[0m"
 echo -e "\e[34mHello in Blue Color\e[0m"
 echo -e "\e[35mHello in Magenta Color\e[0m"
 echo -e "\e[36mHello in Cyan Color\e[0m"
+
+# scalar variables
+b=(10 20)
+echo ${b[0]}
+echo ${b[1]}

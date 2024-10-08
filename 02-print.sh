696613023c2ca1c26675f9a238f38a9443fba731
ArpanaM <muriki.arpana@gmail.com>
2024-10-08 14:58:36 +0530

    Learning colors

--- 02-print.sh
+++ 02-print.sh
@@ -3,3 +3,8 @@ echo starting nginx service
 
 #echo -e "\e[COLmMESSAGE\e[0m"
 echo -e "\e[31mHello in Red Color\e[0m"
+echo -e "\e[32mHello in Blue Color\e[0m"
+echo -e "\e[33mHello in Yellow Color\e[0m"
+echo -e "\e[34mHello in Blue Color\e[0m"
+echo -e "\e[35mHello in Magenta Color\e[0m"
+echo -e "\e[36mHello in Cyan Color\e[0m"
